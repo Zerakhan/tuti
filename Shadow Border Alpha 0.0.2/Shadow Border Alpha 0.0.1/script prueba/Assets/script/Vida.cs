7798341b70f060d50a12082d502dfcbfa795adb1
Zerakhan <Keymaster.1.13.2@gmail.com>
2018-06-03 22:32:37 +0200

    ia fin

---
+++ Shadow Border Alpha 0.0.2/Shadow Border Alpha 0.0.1/script prueba/Assets/script/Vida.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Vida : MonoBehaviour {
+
+bool Life;
+
+	// Use this for initialization
+	void Start () {
+        Life = true;
+	}
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Life = false;
+
+        }
+
+        if (Life == false)
+        {
+            Debug.Log("Scene Loaded");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
